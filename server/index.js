05144bf0b3befa0e3c782968b9eaffe7cc87ee11
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-07-23 10:44:51 +0100

    m

--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var userRoutes = require('./resources/routers/routers');
-var users = require('../database-mongo/index');
+var userRoutes = require("./resources/routers/routers");
+var users = require("../database-mongo/index");
 
 var ctrl = require("../server/resources/controllers/controller");
 var app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client/dist"));
 
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
 app.post("/", (req, res) => {
   ctrl.add(req, res);
 });
-
-
-
 
 app.listen(3000, function () {
   console.log("listening on port 3000!");
--- server/resources/controllers/controller.js
+++ server/resources/controllers/controller.js
@@ -1,53 +1,52 @@
-
-
-
-var User = require('../models/model');
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-
+var User = require("../models/model");
+var bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
-    console.log(req.body)
-    console.log(req.body.email)
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                fullName: req.body.fullName,
-                password: hash,
-                role: req.body.role
-            });
-            console.log(user)
-            user.save()
-                .then(() => res.status(201).json({ message: 'User has been created !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+  console.log(req.body);
+  console.log(req.body.email);
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        password: hash,
+        role: req.body.role,
+      });
+      console.log(user);
+      user
+        .save()
+        .then(() =>
+          res.status(201).json({ message: "User has been created !" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'User NOT FOUND !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Invalid password !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "User NOT FOUND !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Invalid password !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 const Guide = require("../models/model");
@@ -62,12 +61,35 @@ var selectAll = function (callback) {
 };
 var add = function (req, res) {
   Guide.create(
-    { name: req.body.name, description: req.body.description },
+    {
+      name: req.body.name,
+      description: req.body.description,
+      age: req.body.age,
+      gender: req.body.gender,
+      languages: req.body.languages,
+      city: req.body.city,
+      show: true,
+    },
     function (err, small) {
-      console.log("2");
       if (err) console.log("error adding to the database");
       console.log("added successfully to the database");
       res.end();
+    }
+  );
+};
+var select = function (req, res) {
+  Guide.find(
+    {
+      city: req.body.city,
+      gender: req.body.gender,
+      languages: req.body.languages,
+    },
+    function (err, items) {
+      if (err) {
+        callback(err, null);
+      } else {
+        res.send(items);
+      }
     }
   );
 };
