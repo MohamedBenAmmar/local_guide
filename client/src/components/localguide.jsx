0270e6fd924ec888fc203caa398a2841091c8436
Ayman <aymenyaiche@yahoo.fr>
2020-07-23 11:09:00 +0100

    thursday

--- client/src/components/localguide.jsx
+++ client/src/components/localguide.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
- 
+import React from "react";
 
 class FindProfile extends React.Component {
-       constructor(props){
-           super(props)
-         
-        }
-           render (){
-
-              return(
-                <div>
-                      <h1>{this.props.data.Name}</h1>
-                      <h2>{this.props.data.age}</h2>
-                      <h3>{this.props.data.gender}</h3>
-                      <h4>{this.props.data.languages}</h4>
-                                 
-                </div>
-            )
-          } 
-       }
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+  render() {
+    return (
+      <div id="Profile">
+        <img src={this.props.data.imgURL} />
+        <p>Name : {this.props.data.Name}</p>
+        <p>City : {this.props.data.city}</p>
+        <p>Age : {this.props.data.age}</p>
+        <p>Gender :{this.props.data.gender}</p>
+        <p>Languages : {this.props.data.languages.toString()}</p>
+      </div>
+    );
+  }
+}
 export default FindProfile;
--- client/src/components/searchbar.jsx
+++ client/src/components/searchbar.jsx
@@ -1,114 +1,408 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from 'axios';
 
 //import { MDBCol, MDBSelectInput, MDBSelect} from "mdbreact";
-import FindProfile from './localguide.jsx'
-
-
-
-
-
+import FindProfile from "./localguide.jsx";
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataBase: [
+        {
+          Name: "AymanYaich",
+          age: 35,
+          gender: "Male",
+          languages: ["English", "Arabic", "French"],
+          city: "Sousse",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Monastir",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Kairwan",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Sfax",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tunis",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tozeur",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Gafasa",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tataouin",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Gafsa",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Nabeul",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Nabeul",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Tabarka",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+      ],
+      newData= [
+        {
+          Name: "AymanYaich",
+          age: 35,
+          gender: "Male",
+          languages: ["English", "Arabic", "French"],
+          city: "Sousse",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Monastir",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Kairwan",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Sfax",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tunis",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tozeur",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Gafasa",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          city: "Tataouin",
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "AmiraAyoubi",
+          age: 25,
+          gender: "Female",
+          languages: ["English", "Arabic", "Russian"],
+          imgURL:
+            "https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Gafsa",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Nabeul",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Nabeul",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+        {
+          Name: "MounaBrahmi",
+          age: 29,
+          gender: "Female",
+          languages: ["English", "Arabic", "French"],
+          city: "Tabarka",
+          imgURL:
+            "https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg",
+        },
+      ],
+      typingButton: false,
+      selectCity: "selectCity",
+      selectLang: "selectLang",
+      selectGender: "selectGender",
+    };
+    this.filterBy = this.filterBy.bind(this)
+  }
+  typingMethod() {
+    this.setState({
+      typingButton: !this.state.typingButton,
+    });
+   axios.post('/search',{_idOne = this.state.selectCity,
+    _idTwo = this.state.selectLang,
+    _idThree = this.state.selectGender}).then((result)=>{
+      this.setState({newData : result})
+    })
+  }
+  handleChangeCity(e) {
+    this.setState({
+      selectCity: e.target.value,
+    });
+  }
+  handleChangeLang(e) {
+    this.setState({
+      selectLang: e.target.value,
+    });
+  }
+  handleChangeGender(e) {
+    this.setState({
+      selectGender: e.target.value,
+    });
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            dataBase:[
-                {Name : "AymanYaich",age : 37 , gender : "Male", languages:["English","Arabic","French"],imgURL:"https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg"},
-                {Name : "AmiraAyoubi",age :25 , gender : "Female", languages:["English","Arabic","Russian"],imgURL:"https://premiumaddons.b-cdn.net/wp-content/uploads/2018/01/beautiful-woman-smiles.jpg"},
-                {Name : "MounaBrahmi",age : 29 , gender : "Female", languages:["English","Arabic","French"],imgURL:"https://www.yourdictionary.com/images/definitions/lg/10750.person.jpg"},
-                {Name : "MouradBenAmmar",age : 31 , gender : "Male", languages:["English","Arabic","French","Spanish","Portuguess"],imgURL:"https://i.ndtvimg.com/i/2016-12/vivek-ramaswamy-650-400_650x400_61481621830.jpg."}
-                 ] ,
-                 typingButton : false
-                }
+  // filterBy() {
+  
 
-                
-        }
-        
-    typingMethod (){
-        this.setState ({
-            typingButton : ! this.state.typingButton
-        })
-    }
-   
-     render(){
-         if (!this.state.typingButton){
+  //   var _idOne = this.state.selectCity;
+  //   var _idTwo = this.state.selectLang;
+  //   var _idThree = this.state.selectGender;
+  //   let dataFilter = this.state.dataBase.filter((ele) => {
+  //     return (
+  //       ele.city === _idOne &&
+  //       ele.languages.includes(_idTwo) &&
+  //       ele.gender === _idThree
+  //     );
+  //   });
+  //   this.setState({
+  //     newData:dataFilter,
+  //   })
+  // }
+
+  render() {
+    if (!this.state.typingButton) {
       return (
-          
-         <div >
-           
-           <div id="brand">
-           <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-         <div className="navbar-header">
-          <a className="navbar-brand" href="#">Find your local guide</a>
-           </div>
-           </div>
+        <div>
+          <div id="brand">
+            <nav className="navbar navbar-inverse">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <a className="navbar-brand" href="#">
+                    Find your local guide
+                  </a>
+                </div>
+              </div>
             </nav>
-           </div>
+          </div>
           <div id="searchBar">
-          <select className="browser-default custom-select custom-select-lg mb-3">
-                  
-                  <option defaultValue  >Cities</option>
-                  <option value="1">Bizete</option>
-                  <option value="2">Djerba/Zarzis/Medenin</option>
-                  <option value="3">Gabes/Matmata</option>
-                  <option value="4">Gasserine/Sebitla/Dogga</option>
-                  <option value="5">Kairawen</option>
-                  <option value="6">Kebili</option>
-                  <option value="7">Kef</option>
-                  <option value="8">Nabel/Hammamet/Klébia</option>
-                  <option value="9">Sfax/Kerkenah</option>
-                  <option value="10">Siliana/Beja</option>
-                  <option value="11">Sousse/Monastir/Mahdia</option>
-                  <option value="12">Tabarka/Ain-Drahem/Jendouba</option>
-                  <option value="13">Tataouin</option>
-                  <option value="14">Tozeur/Gafsa</option>
-                  <option value="15">Tunis</option>
-                  <option value="16">Zaghwan</option>
-                
-                  </select>
-           <select className="custom-select custom-select-sm">
-                 <option defaultValue>Languages</option>
-                 <option value="1">Arabic</option>
-                 <option value="1">Chinese</option>                                      
-                 <option value="2">English</option>
-                 <option value="3">French</option>
-                 <option value="4">German</option>
-                 <option value="6">Italian</option>
-                 <option value="8">Portuguese</option>
-                 <option value="9">Russian</option>
-                 <option value="10">Spanish</option>
-                 
-                 </select>
-         <select className="custom-select custom-select-sm">
-                 <option defaultValue>Gender</option>
-                 <option value="1">Female</option>
-                 <option value="2">Male</option>
-           </select> 
-           </div>  
-           {/* <button type="button" id="buttonFind">Find</button> */}
-           <div id="buttonFind" >
-           <button type="button" className="btn btn-primary" onClick={this.typingMethod.bind(this)}>Search</button>
-           </div>
-           </div>
-      )
-         }else{
-           return (
-               <div>
-                   hellooo Renderrrrrr
-                   
-                   { this.state.dataBase.map(data=>{
-                       <FindProfile data={data}/>
-                   })} 
-               </div>
-           )
-               
-           
-         }
-      
-     }
- }
+            <select
+              className="browser-default custom-select custom-select-lg mb-3"
+              onChange={this.handleChangeCity.bind(this)}
+            >
+              <option defaultValue>Cities</option>
+              <option value="Bizerte">Bizete</option>
+              <option value="Djerba/Zarzis/Medenin">
+                Djerba/Zarzis/Medenin
+              </option>
+              <option value="Gabes/Matmata">Gabes/Matmata</option>
+              <option value="Gasserine/Sebitla/Dogga">
+                Gasserine/Sebitla/Dogga
+              </option>
+              <option value="Kairawen">Kairawen</option>
+              <option value="Kebili">Kebili</option>
+              <option value="Kef">Kef</option>
+              <option value="Nabel/Hammamet/Klébia">
+                Nabel/Hammamet/Klébia
+              </option>
+              <option value="Sfax/Kerkenah">Sfax/Kerkenah</option>
+              <option value="Siliana/Beja">Siliana/Beja</option>
+              <option value="Sousse/Monastir/Mahdia">
+                Sousse/Monastir/Mahdia
+              </option>
+              <option value="Tabarka/Ain-Drahem/Jendouba">
+                Tabarka/Ain-Drahem/Jendouba
+              </option>
+              <option value="Tataouin">Tataouin</option>
+              <option value="Tozeur/Gafsa">Tozeur/Gafsa</option>
+              <option value="Tunis">Tunis</option>
+              <option value="Zaghwan">Zaghwan</option>
+            </select>
+            <select
+              className="custom-select custom-select-sm"
+              onChange={this.handleChangeLang.bind(this)}
+            >
+              <option defaultValue>Languages</option>
+              <option value="Arabic">Arabic</option>
+              <option value="Chinese">Chinese</option>
+              <option value="English">English</option>
+              <option value="French">French</option>
+              <option value="German">German</option>
+              <option value="Italian">Italian</option>
+              <option value="Portuguese">Portuguese</option>
+              <option value="Russian">Russian</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+            <select
+              className="custom-select custom-select-sm"
+              onChange={this.handleChangeGender.bind(this)}
+            >
+              <option defaultValue>Gender</option>
+              <option value="Female">Female</option>
+              <option value="Male">Male</option>
+            </select>
+          </div>
+          {/* <button type="button" id="buttonFind">Find</button> */}
+          <div id="buttonFind">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.typingMethod.bind(this)}
+            >
+              Search
+            </button>
+          </div>
+          <p>{this.state.selectCity}</p>
+          <p>{this.state.selectLang}</p>
+          <p>{this.state.selectGender}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div id="wrapper">
+            {this.state.newData.map((data) => {
+              return (
+                <table>
+                  <tr>
+                    <th>
+                      <FindProfile data={data} />
+                    </th>
+                  </tr>
+                </table>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
- export default Search
+export default Search;
